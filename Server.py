c749a8529a9794505f5a95073da048996e22787d
Ivan <juniorivandias@gmail.com>
2019-05-09 21:44:22 -0300

    chat

--- Server.py
+++ Server.py
@@ -6,7 +6,7 @@ from sqlalchemy import  Column, Integer, String, Boolean
 # PATH = 'postgresql://postgres:postgres@localhost/bikerepair'
 
 # Heroku
- PATH = 'postgres://upgiusttrivecz:3b66b747df53cd9b4a5400aafe3d8ade87700be26ca00104ed89e1466e5d413e@ec2-54-235-208-103.compute-1.amazonaws.com:5432/dfhi7hgvmpvr1r'
+PATH = 'postgres://upgiusttrivecz:3b66b747df53cd9b4a5400aafe3d8ade87700be26ca00104ed89e1466e5d413e@ec2-54-235-208-103.compute-1.amazonaws.com:5432/dfhi7hgvmpvr1r'
 
 App = Flask(__name__)
 App.config['SQLALCHEMY_DATABASE_URI'] = PATH
